56e4853e3229a3255677039e0a571081d27602d9
Mehtab Alam <mehtabalam6151@gmail.com.com>
2025-11-24 09:06:44 +0530

    New Questions Added

--- server/index.js
+++ server/index.js
@@ -29,106 +29,105 @@ app.get('/api/seed', async (req, res) => {
         await Question.deleteMany({}); 
 
         const questions = [
-            {
-                questionText: "1. What does HTML stand for?",
-                options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Tool Multi Language"],
-                correctAnswer: "Hyper Text Markup Language"
+            { 
+                questionText: "Inside which HTML element do we put the JavaScript?", 
+                options: ["<javascript>", "<scripting>", "<script>", "<js>"], 
+                correctAnswer: "<script>" 
             },
-            {
-                questionText: "2. Who is making the Web standards?",
-                options: ["Mozilla", "Microsoft", "The World Wide Web Consortium (W3C)", "Google"],
-                correctAnswer: "The World Wide Web Consortium (W3C)"
+            { 
+                questionText: "Where is the correct place to insert a JavaScript?", 
+                options: ["The <head> section", "The <body> section", "Both", "None"], 
+                correctAnswer: "Both" 
             },
-            {
-                questionText: "3. Choose the correct HTML element for the largest heading:",
-                options: ["<heading>", "<h6>", "<head>", "<h1>"],
-                correctAnswer: "<h1>"
+            { 
+                questionText: "What is the correct syntax for referring to an external script called 'xxx.js'?", 
+                options: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"], 
+                correctAnswer: "<script src='xxx.js'>" 
             },
-            {
-                questionText: "4. What is the correct HTML element for inserting a line break?",
-                options: ["<lb>", "<br>", "<break>", "<b>"],
-                correctAnswer: "<br>"
+            { 
+                questionText: "How do you write 'Hello World' in an alert box?", 
+                options: ["msg('Hello World');", "alert('Hello World');", "msgBox('Hello World');", "alertBox('Hello World');"], 
+                correctAnswer: "alert('Hello World');" 
             },
-            {
-                questionText: "5. Which character is used to indicate an end tag?",
-                options: ["*", "/", "<", "^"],
-                correctAnswer: "/"
+            { 
+                questionText: "How do you create a function in JavaScript?", 
+                options: ["function myFunction()", "function:myFunction()", "create myFunction()", "function = myFunction()"], 
+                correctAnswer: "function myFunction()" 
             },
-            {
-                questionText: "6. How can you open a link in a new tab/browser window?",
-                options: ["<a href='url' target='new'>", "<a href='url' target='_blank'>", "<a href='url' new>", "<a href='url' target='window'>"],
-                correctAnswer: "<a href='url' target='_blank'>"
+            { 
+                questionText: "How do you call a function named 'myFunction'?", 
+                options: ["call function myFunction()", "call myFunction()", "myFunction()", "Call.myFunction()"], 
+                correctAnswer: "myFunction()" 
             },
-            {
-                questionText: "7. Which of these elements are all <table> elements?",
-                options: ["<table> <head> <tfoot>", "<table> <tr> <td>", "<table> <tr> <tt>", "<thead> <body> <tr>"],
-                correctAnswer: "<table> <tr> <td>"
+            { 
+                questionText: "How to write an IF statement in JavaScript?", 
+                options: ["if i = 5 then", "if i == 5 then", "if (i == 5)", "if i = 5"], 
+                correctAnswer: "if (i == 5)" 
             },
-            {
-                questionText: "8. How can you make a numbered list?",
-                options: ["<dl>", "<ol>", "<list>", "<ul>"],
-                correctAnswer: "<ol>"
+            { 
+                questionText: "How to write an IF statement if 'i' is NOT equal to 5?", 
+                options: ["if (i <> 5)", "if (i != 5)", "if i =! 5 then", "if (i not 5)"], 
+                correctAnswer: "if (i != 5)" 
             },
-            {
-                questionText: "9. What is the correct HTML for making a checkbox?",
-                options: ["<input type='check'>", "<check>", "<checkbox>", "<input type='checkbox'>"],
-                correctAnswer: "<input type='checkbox'>"
+            { 
+                questionText: "How does a WHILE loop start?", 
+                options: ["while (i <= 10; i++)", "while (i <= 10)", "while i = 1 to 10", "while (i++ <= 10)"], 
+                correctAnswer: "while (i <= 10)" 
             },
-            {
-                questionText: "10. Which HTML attribute specifies an alternate text for an image?",
-                options: ["title", "src", "alt", "longdesc"],
-                correctAnswer: "alt"
+            { 
+                questionText: "How does a FOR loop start?", 
+                options: ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)"], 
+                correctAnswer: "for (i = 0; i <= 5; i++)" 
             },
-
-            {
-                questionText: "11. What does CSS stand for?",
-                options: ["Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
-                correctAnswer: "Cascading Style Sheets"
+            { 
+                questionText: "How can you add a comment in a JavaScript?", 
+                options: ["'This is a comment", "", "//This is a comment", "*This is a comment"], 
+                correctAnswer: "//This is a comment" 
             },
-            {
-                questionText: "12. Which HTML tag is used to define internal CSS?",
-                options: ["<css>", "<script>", "<style>", "<design>"],
-                correctAnswer: "<style>"
+            { 
+                questionText: "How to insert a comment that has more than one line?", 
+                options: ["/* Comment */", "// Comment //", "", "# Comment"], 
+                correctAnswer: "/* Comment */" 
             },
-            {
-                questionText: "13. Which is the correct CSS syntax?",
-                options: ["body {color: black;}", "{body;color:black;}", "body:color=black;", "body = color: black;"],
-                correctAnswer: "body {color: black;}"
+            { 
+                questionText: "What is the correct way to write a JavaScript array?", 
+                options: ["var c = (1:'red', 2:'green')", "var c = ['red', 'green']", "var c = 'red', 'green'", "var c = 1 = ('red')"], 
+                correctAnswer: "var c = ['red', 'green']" 
             },
-            {
-                questionText: "14. How do you insert a comment in a CSS file?",
-                options: ["// this is a comment", "/* this is a comment */", "' this is a comment", "// this is a comment //"],
-                correctAnswer: "/* this is a comment */"
+            { 
+                questionText: "How do you round the number 7.25, to the nearest integer?", 
+                options: ["Math.round(7.25)", "round(7.25)", "rnd(7.25)", "Math.rnd(7.25)"], 
+                correctAnswer: "Math.round(7.25)" 
             },
-            {
-                questionText: "15. Which property is used to change the background color?",
-                options: ["color", "bgcolor", "background-color", "bg-color"],
-                correctAnswer: "background-color"
+            { 
+                questionText: "How do you find the number with the highest value of x and y?", 
+                options: ["ceil(x, y)", "Math.ceil(x, y)", "Math.max(x, y)", "top(x, y)"], 
+                correctAnswer: "Math.max(x, y)" 
             },
-            {
-                questionText: "16. How do you select an element with id 'demo'?",
-                options: ["#demo", ".demo", "demo", "*demo"],
-                correctAnswer: "#demo"
+            { 
+                questionText: "What is the correct JavaScript syntax for opening a new window?", 
+                options: ["w2 = window.new('url');", "w2 = window.open('url');", "w2 = window.create('url');", "w2 = window.window('url');"], 
+                correctAnswer: "w2 = window.open('url');" 
             },
-            {
-                questionText: "17. How do you select elements with class name 'test'?",
-                options: ["#test", ".test", "test", "*test"],
-                correctAnswer: ".test"
+            { 
+                questionText: "JavaScript is the same as Java.", 
+                options: ["True", "False", "Maybe", "Sometimes"], 
+                correctAnswer: "False" 
             },
-            {
-                questionText: "18. Which property controls the text size?",
-                options: ["font-style", "text-size", "font-size", "text-style"],
-                correctAnswer: "font-size"
+            { 
+                questionText: "How can you detect the client's browser name?", 
+                options: ["navigator.appName", "browser.name", "client.navName", "window.browser"], 
+                correctAnswer: "navigator.appName" 
             },
-            {
-                questionText: "19. How do you make each word in a text start with a capital letter?",
-                options: ["text-transform: capitalize", "text-style: capitalize", "transform: capitalize", "font-transform: capitalize"],
-                correctAnswer: "text-transform: capitalize"
+            { 
+                questionText: "Which event occurs when the user clicks on an HTML element?", 
+                options: ["onmouseclick", "onchange", "onclick", "onmouseover"], 
+                correctAnswer: "onclick" 
             },
-            {
-                questionText: "20. How do you display hyperlinks without an underline?",
-                options: ["text-decoration: no-underline", "text-decoration: none", "underline: none", "decoration: no-underline"],
-                correctAnswer: "text-decoration: none"
+            { 
+                questionText: "How do you declare a JavaScript variable?", 
+                options: ["v carName;", "var carName;", "variable carName;", "val carName;"], 
+                correctAnswer: "var carName;" 
             }
         ];
 
